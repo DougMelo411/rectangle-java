64ac30da501a136e4c57986a6e596b14bd594a25
Douglas Melo <dougmelo411vm@outlook.com>
2022-08-16 13:29:45 -0300

    Calculo retangulo

---
+++ src/entites/Rectangle.java
@@ -0,0 +1,21 @@
+package entites;
+
+public class Rectangle {
+
+	public double width;
+	public double height;
+
+	public double area() {
+		return height * width;
+
+	}
+
+	public double perimeter() {
+		return 2 * (height + width);
+	}
+
+	public double diagonal() {
+		return Math.sqrt(width * width + height * height);
+	}
+
+}
